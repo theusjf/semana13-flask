2b73bf90880784b213e5975f8755c35b1386021b
theusjf <matheusdejesusf@gmail.com>
2025-01-08 18:33:35 +0000

    commit 1

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+    email = EmailField('Qual é o seu email (Envio de notificação do novo usuário)?', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
-from ..models import User
+from ..models import User, Role
 from ..email import send_email, send_simple_message
 from . import main
 from .forms import NameForm
@@ -9,10 +9,12 @@ from .forms import NameForm
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    user_all = User.query.all();
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            user_role = Role.query.filter_by(name='User').first();
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -28,4 +30,4 @@ def index():
         return redirect(url_for('.index'))
     return render_template('index.html',
                            form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False), user_all=user_all)
